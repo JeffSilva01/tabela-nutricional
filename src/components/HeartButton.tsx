d4f62ea1ae97f5e16aec28e27e24cb6d313cfec0
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-09-06 23:47:23 -0300

    fix prettier

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -9,7 +9,7 @@ export function HeartButton() {
     <button
       type="button"
       onClick={() => setIsLiked(!isLiked)}
-      className="bg-primary-200 m-4 flex h-10 w-10 items-center justify-center rounded-full shadow-[0px_2px_4px_0px_rgba(59,_171,_30,_0.25)]"
+      className="m-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-200 shadow-[0px_2px_4px_0px_rgba(59,_171,_30,_0.25)]"
     >
       {isLiked ? <Heart fill="" /> : <Heart />}
     </button>
